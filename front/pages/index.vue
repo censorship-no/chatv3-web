c436ce4611fb3281bc22f150a74c405a12c404b0
Your Name <you@example.com>
2022-03-04 17:43:02 +0300

    ADD matrix moscow server

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -5,18 +5,18 @@
       <div class="container px-3 sm:px-0">
         <p class="text-lg">{{ $t("about.info") }}</p>
         <p class="text-lg">{{ $t("about.sites") }}</p>
-        <!-- <p
+        <p
           class="
-            grid grid-cols-3
+            flex
             gap-3
-            sm:gap-8
             text-center
-            justify-around
+            justify-center
             my-8
           "
         >
           <a
             class="
+            min-w-[200px]
               sm:text-xl
               border-2 border-gray-600
               p-3
@@ -25,10 +25,10 @@
               text-gray-600
               hover:border-[#e53323] hover:text-[#e53323]
             "
-            href="https://kyiv.dcomm.net.ua/"
-            >{{ $t("cities.kyiv") }}</a
+            href="https://moscow.chatv3.ru"
+            >{{ $t("cities.moscow") }}</a
           >
-          <a
+          <!-- <a
             class="
               sm:text-xl
               border-2 border-gray-600
@@ -53,9 +53,9 @@
             "
             href="https://kharkiv.dcomm.net.ua"
             >{{ $t("cities.kharkiv") }}</a
-          >
+          > -->
         </p>
-        <p class="text-gray-400 text-center">{{ $t("about.local") }}</p> -->
+        <p class="text-gray-400 text-center">{{ $t("about.local") }}</p>
       </div>
     </div>
     <main class="container px-3 sm:px-0">
